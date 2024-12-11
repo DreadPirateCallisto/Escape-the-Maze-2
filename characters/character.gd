29dadb265a7de7d638b67612fff1e0b7aee0b79c
DreadPirateCallisto <mzee.radi77@icloud.com>
2024-12-11 07:38:41 +0200

    player scene

--- characters/character.gd
+++ characters/character.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
-@export var speed: int
+var animation_speed = 3
+@onready var tween
 
 var tile_size = 64
 var moving = false
 var facing = 'right'
 @onready var ray = $RayCast2D
+
 var inputs = {
 	"right": Vector2.RIGHT,
 	"left": Vector2.LEFT,
@@ -19,27 +21,19 @@ func _ready():
 	# adding a half-tile amount to make sure the player is centered on the tile.
 	position += Vector2.ONE * tile_size/2
 	
-func _unhandled_input(event):
-	if moving:
-		return
-	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
-			move(dir)
-			
 func move(dir):
-	$AnimationPlayer.speed_scale = speed
+	$AnimationPlayer.speed_scale = animation_speed
 	ray.target_position = inputs[dir] * tile_size
 	ray.force_raycast_update()
 	if !ray.is_colliding():
 		#position += inputs[dir] * tile_size
-		$AnimationPlayer.play(inputs[dir])
-		var tween = create_tween()
-		tween.tween_property(
-			self,
-			"position",
-			position+inputs[dir]*tile_size,
-			1.0/speed
-		).set_trans(Tween.TRANS_SINE)
+		$AnimationPlayer.play(dir)
+		tween = create_tween()
+		tween.tween_property(self, "position",
+		position+inputs[dir]*tile_size,
+		1.0/animation_speed
+		).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 		moving = true
-		await  tween.finished
+		await tween.finished
 		moving = false
+	return true
